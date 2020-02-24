728b4552374533947f0f00f4a1095716de0b4cc4
EkaterinaKozlova <zireael6@gmail.com>
2020-02-24 17:16:35 +0300

    Text and key from files, refactoring.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <streambuf>
 #include "blowfish.h"
 #include "xor.h"
 
 using namespace std;
 
-int main(int argc, const char * argv[])
-{
+void getFromFiles(std::string& text, std::string& key){
+    ifstream testText("text.txt");
+    ifstream keyText("key.txt");
+    string _key((std::istreambuf_iterator<char>(keyText)),
+            std::istreambuf_iterator<char>());
+    string _text((std::istreambuf_iterator<char>(testText)),
+            std::istreambuf_iterator<char>());
+    key = _key;
+    text = _text;
+}
+
+void XORTest(std::string& text, std::string& key){
     XOR x;
-    string key = "1234r32lmweflkmewfflowkem";
-    string text = "The length of a C string is determined by the terminating null-character: A C string is as long as the number of characters between the beginning of the string and the terminating null character (without including the terminating null character itself).";
-    unsigned char blowfish_key[key.length()];
-    unsigned char blowfish_text[text.length()];
-    std::copy( key.begin(), key.end(), blowfish_key );
-    std::copy( text.begin(), text.end(), blowfish_text );
     string encrypted = x.encryptOrDectypt(text,key);
     cout << "Encrypted XOR:" << encrypted << "\n";
     string decrypted = x.encryptOrDectypt(encrypted,key);
     cout << "Decrypted XOR:" << decrypted << "\n";
+}
+
+void BlowfishTest(std::string& text, std::string& key){
+    unsigned char blowfish_key[key.length()];
+    unsigned char blowfish_text[text.length()];
+    std::copy( key.begin(), key.end(), blowfish_key );
+    std::copy( text.begin(), text.end(), blowfish_text );
     Blowfish blowfish;
     blowfish.SetKey(blowfish_key, sizeof(key));
     blowfish.Encrypt(blowfish_text, blowfish_text, sizeof(blowfish_text));
@@ -24,5 +37,13 @@ int main(int argc, const char * argv[])
     cout << "Encrypted Blowfish:" << blowfish_text << "\n";
     blowfish.Decrypt(blowfish_text, blowfish_text, sizeof(blowfish_text));
     cout << "Decrypted Blowfish:" << blowfish_text << "\n";
+}
+
+int main(int argc, const char * argv[])
+{
+    std::string key,text;
+    getFromFiles(text,key);
+    XORTest(text,key);
+    BlowfishTest(text,key);
     return 0;
 }
